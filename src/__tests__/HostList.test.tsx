e8a10694bb069e786c722f2b3ef3231e1a5bf7b0
MMFane <sam.erin.williams@gmail.com>
2025-02-21 14:12:15 -0800

    improve test async behavior and add tests for filtering and querying

--- src/__tests__/HostList.test.tsx
+++ src/__tests__/HostList.test.tsx
@@ -1,17 +1,65 @@
-import { describe, expect, test, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  MockInstance,
+  test,
+  vi,
+} from "vitest";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { dataService } from "../services/dataService";
 import { paginatedHosts } from "./fixtures/fixtures";
 import HostList from "../components/HostList";
 
 describe("HostList", () => {
-  test("HostCard renders expected data", () => {
-    const fetchHostsSpy = vi
-      .spyOn(dataService, 'fetchHosts')
+  let fetchHostsSpy: MockInstance;
+
+  beforeEach(() => {
+    fetchHostsSpy = vi
+      .spyOn(dataService, "fetchHosts")
       .mockImplementation(async () => paginatedHosts);
     render(<HostList />);
-    expect(fetchHostsSpy).toHaveBeenCalled()
-    expect(screen.getByText("Host List"));
-    expect(screen.getByText("Load More"));
+  });
+
+  afterEach(() => {
+    fetchHostsSpy.mockRestore();
+  });
+
+  test("HostList renders expected data", async () => {
+    await waitFor(() => {
+      expect(fetchHostsSpy).toHaveBeenCalledWith("", "");
+      expect(screen.getByText("Host List"));
+      expect(screen.getByText("Filter Hosts"));
+      expect(screen.getByText("Load More"));
+      expect(screen.getByText("1.2.3.4.5"));
+    });
+  });
+
+  test("HostList loads more", async () => {
+    const loadMoreBtn = await screen.findByText("Load More");
+    act(() => {
+      fireEvent.click(loadMoreBtn);
+    });
+    await waitFor(() => {
+      expect(fetchHostsSpy).toHaveBeenCalledWith("abc123", "");
+    });
+  });
+
+  test("HostList can query", async () => {
+    const queryInput = await screen.findByLabelText("Filter Hosts");
+    act(() => {
+      userEvent.type(queryInput, "xyz");
+    });
+    await waitFor(() => {
+      expect(fetchHostsSpy).toHaveBeenCalledWith("", "xyz");
+    });
   });
 });
