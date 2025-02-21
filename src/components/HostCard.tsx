287b5c04424260cf3fe59315d4080ffad71825e5
MMFane <sam.erin.williams@gmail.com>
2025-02-21 09:47:09 -0800

    aggregate host protocols by extended_service_name

--- src/components/HostCard.tsx
+++ src/components/HostCard.tsx
@@ -1,13 +1,40 @@
+import { useState, useEffect } from "react";
 import type { Host } from "../types/types";
+import countBy from "lodash/countBy";
+import { Dictionary } from "lodash";
 
 interface HostProps {
   host: Host;
 }
 
 function HostCard({ host }: HostProps) {
+  const [aggregatedServiceNames, setAggregatedServiceNames] =
+    useState<Dictionary<number>>();
+
+  useEffect(() => {
+    const serviceNames = host.services.map(
+      (service) => service.extended_service_name
+    );
+    const aggregatedServiceNames = countBy(serviceNames);
+
+    setAggregatedServiceNames(aggregatedServiceNames);
+  }, [host]);
+
   return (
     <>
-      <li>{host.ip}</li>
+      <li>
+        <p>{host.ip}</p>
+        <ul>
+          {aggregatedServiceNames &&
+            Object.entries(aggregatedServiceNames).map((entry) => {
+              return (
+                <li key={entry[0]}>
+                  {entry[0]}: {entry[1]}
+                </li>
+              );
+            })}
+        </ul>
+      </li>
     </>
   );
 }
